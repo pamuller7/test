431b54380320aa7d60cb84a16f48812b8919ab3d
Paul Muller <pamuller@z4r4p1.42lyon.fr>
2026-03-18 14:56:35 +0100

    test

---
+++ ft.py
@@ -0,0 +1,12 @@
+def mul(x, y):
+	return (x * y)
+
+
+def swap(x, y):
+	stock = x
+	x = y
+	y = stock
+	return(x, y)
+
+def ret_ascii(c):
+	return(ord(c))
---
+++ test.py
@@ -0,0 +1,33 @@
+from ft import mul, swap, ret_ascii
+import unittest
+
+class Testing_ft(unittest.TestCase):
+	def test_mul(self):
+		self.assertEqual(mul(3, 2), 6)
+		self.assertEqual(mul(0, 651), 0)
+		self.assertEqual(mul(3, -2), -6)
+
+	def test_swap(self):
+		self.assertEqual(swap(5, 9), (9, 5))
+		self.assertEqual(swap('a', 'b'), ('b','a'))
+	
+	def test_ret_ascii(self):
+		self.assertEqual(ret_ascii('a'), 97)
+		self.assertEqual(ret_ascii('0'), 48)
+
+class Testing_ft2(unittest.TestCase):
+	def test_mul(self):
+		self.assertEqual(mul(8, 1), 8)
+		self.assertEqual(mul(0, 1), 0)
+		self.assertEqual(mul(1, -6), -6)
+
+	def test_swap(self):
+		self.assertEqual(swap(5, 9), (9, 5))
+		self.assertEqual(swap('a', 'b'), ('b','a'))
+	
+	def test_ret_ascii(self):
+		self.assertEqual(ret_ascii('a'), 97)
+		self.assertEqual(ret_ascii('0'), 48)
+
+# if __name__ == '__main__':
+# 	unittest.main()
